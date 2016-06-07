498975ea0ec01f76558b35962e99dee0788cc380
Matheus Paiva <matheus.a.paiva@gmail.com>
2016-06-06 23:56:33 -0300

    Mockup Inicial

---
+++ app/pages/visit-report-list/visit-report-list.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/visit-report-list/visit-report-list.html'
+})
+export class VisitReportList {
+  constructor() {
+
+  }
+}
