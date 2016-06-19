b9f487abcdddf7620d7405dd457aeb14f41b7f5f
Matheus Paiva <matheus.a.paiva@gmail.com>
2016-06-18 21:39:26 -0300

    Criado recurso para tirar foto de nota do reembolso

--- app/pages/newReport/newReport.js
+++ app/pages/newReport/newReport.js
@@ -1,17 +1,18 @@
-import {Page, NavController} from "ionic-angular";
+import {Page, NavController, Alert, ActionSheet, Platform} from "ionic-angular";
 import * as dateformatAll from 'dateformat';
+import {Camera} from 'ionic-native';
 
 @Page({
   templateUrl: 'build/pages/newReport/newReport.html'
 })
 export class NewReport {
   static get parameters() {
-    return [[NavController]];
+    return [[NavController], [Platform]];
   }
 
-  constructor(nav) {
+  constructor(nav, platform) {
     this.nav = nav;
-    this.selectedItem = false;
+    this.platform = platform;
 
     let dateformat = dateformatAll.default;
 
@@ -19,7 +20,54 @@ export class NewReport {
       description: CONFIGS.defaultValues.reportDescription,
       date: dateformat(new Date(), 'yyyy-mm-dd'),
       meal: CONFIGS.defaultValues.launchPrice,
-      othersExpenses: CONFIGS.defaultValues.transportPrice
+      othersExpenses: CONFIGS.defaultValues.transportPrice,
+      image: {
+        title: 'Imagem de nota de reembolso',
+        data: null
+      }
     };
+  }
+
+  pickImage() {
+    Camera.getPicture({ destinationType: Camera.DestinationType.DATA_URL }).then((imageData) => {
+      this.newReport.image.data = 'data:image/jpeg;base64,' + imageData;
+    },
+    (err) => {
+
+      if (err != 'Camera cancelled') {
+        let confirm = Alert.create({
+          title: 'Erro obtendo imagem',
+          message: err,
+          buttons: ['OK']
+        });
+
+        this.nav.present(confirm);
+      }
+    });
+  }
+
+  openImageActionSheet() {
+    let actionSheet = ActionSheet.create({
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Tirar outra foto',
+          icon: !this.platform.is('ios') ? 'camera' : null,
+          handler: () => {
+            this.pickImage()
+          }
+        },
+        {
+          text: 'Deletar',
+          role: 'destructive',
+          icon: !this.platform.is('ios') ? 'trash' : null,
+          handler: () => {
+            this.newReport.image.data = null;
+          }
+        }
+      ]
+    });
+
+    this.nav.present(actionSheet);
   }
 }
